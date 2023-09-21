d06d871ba81758dd0b608c1f929b6412e523383c
Abimbola Khalilur-Rahman <khalilrahmanabimbola@gmail.com>
2023-09-21 10:00:30 +0100

    updated with while statement

--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
 ﻿// code to write a simple dice game
 
-Console.WriteLine("===Simple Dice Game=== \nType 'Start' or 'S' to begin ");
+Console.Write("===Simple Dice Game=== \nType 'Start' or 'S' to begin: ");
 string  startGame = Console.ReadLine()!;
 
 const int doublePoint = 2;
 const int triplePoint = 6;
 int totalPoint = 0;
 
-if (!(startGame.Equals("Start", StringComparison.OrdinalIgnoreCase) || startGame.Equals("S", StringComparison.OrdinalIgnoreCase)))
+while (!(startGame.Equals("Start", StringComparison.OrdinalIgnoreCase) || startGame.Equals("S", StringComparison.OrdinalIgnoreCase)))
 {
-    Console.WriteLine("Type 'Start' or 'S'to start the game properly");
-    return;
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Invalid key word inserted");
+    Console.ResetColor();
+    Console.Write("Type 'Start' or 'S' to begin: ");
+    startGame = Console.ReadLine()!;
 }
+
+// if (!(startGame.Equals("Start", StringComparison.OrdinalIgnoreCase) || startGame.Equals("S", StringComparison.OrdinalIgnoreCase)))
+// {
+//     Console.WriteLine("Type 'Start' or 'S'to start the game properly");
+//     return;
+// }
 
 Random rand = new();
 int diceOne = rand.Next(1, 7);
@@ -42,36 +51,36 @@ else
     totalPoint += originalScore;
     Console.WriteLine($"total point: {totalPoint}");
 }
-
+int luckyNumber = 21;
 
 // assign gift to certain totalpoints
 if(totalPoint >=22 && totalPoint <=24)
 {
-    Console.WriteLine("You won all the available gifts ");
+    Console.WriteLine("Congratulations!! You won all the available gifts ");
 }
 
-int luckyNumber = 21;
-if (totalPoint == luckyNumber)
+
+else if (totalPoint == luckyNumber)
 {
-    Console.WriteLine("You got a lucky number, You won a Samsung Galaxy A24");
+    Console.WriteLine("Congratulations!! You got a lucky number, You won a Samsung Galaxy A24");
 }
 
-if (totalPoint >= 18 && totalPoint <= 20 )
+else if (totalPoint >= 18 && totalPoint <= 20 )
 {
     Console.WriteLine("You won a ticket to Silverbird's Cinema");
 }
 
-if (totalPoint == 17)
+else if (totalPoint == 17)
 {
     Console.WriteLine("You won a 5000mAh power bank ");
 }
 
-if (totalPoint >= 14 && totalPoint <=16 )
+else if (totalPoint >= 14 && totalPoint <=16 )
 {
     Console.WriteLine("You won a Notebook");
 }
 
-if (totalPoint >= 10 && totalPoint <= 13)
+else if (totalPoint >= 10 && totalPoint <= 13)
 {
     Console.WriteLine("You won a balloon");
 }
